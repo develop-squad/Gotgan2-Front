dd4b99956902f4490cfa2fe438aea4f8765f6d73
MJ <codemike@naver.com>
2020-11-29 13:11:52 +0900

    refactor - 유저 관리 페이지 관련 리팩토링

--- src/components/Tables/UserTable.vue
+++ src/components/Tables/UserTable.vue
@@ -3,25 +3,25 @@
     <md-table>
       <md-table-row>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "User Index" : "유저 인덱스"
+          this._props.englishSwitch ? "User Index" : "유저 인덱스"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "ID" : "ID"
+          this._props.englishSwitch ? "ID" : "ID"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "Name" : "이름"
+          this._props.englishSwitch ? "Name" : "이름"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "Group" : "소속"
+          this._props.englishSwitch ? "Group" : "소속"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "Authority" : "권한"
+          this._props.englishSwitch ? "Authority" : "권한"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "StudentID" : "학번"
+          this._props.englishSwitch ? "StudentID" : "학번"
         }}</md-table-head>
         <md-table-head>{{
-          this._props.englishSwitch_Table ? "Registration Date" : "가입일"
+          this._props.englishSwitch ? "Registration Date" : "가입일"
         }}</md-table-head>
       </md-table-row>
 
@@ -44,41 +44,34 @@ import axios from "axios";
 export default {
   name: "stock-table",
   props: {
-    userInfo_Table: Object,
-    englishSwitch_Table: Boolean,
+    englishSwitch: Boolean,
   },
   data() {
     return {
       userList: [],
     };
   },
-  created() {
+  mounted() {
     this.getUserList();
-
-    this.exportData();
   },
   methods: {
-    exportData: function () {
-      var userParams = new URLSearchParams();
-      var vue = this;
-
-      userParams.append("session", vue.getCookie("session"));
+    getUserList() {
+      let userParams = new URLSearchParams();
+      userParams.append("session", this.getSession());
 
       axios
         .post("https://api.devx.kr/GotGan/v1/user_list.php", userParams)
-        .then(function (response) {
-          console.log(response.data);
-          for (var x = 0; x < Object.keys(response.data.users).length; x++) {
-            vue.userList.push(response.data.users[x]);
-          }
+        .then((response) => {
+          response.data.users.forEach((el) => {
+            this.userList.push(el);
+          });
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log(error);
         });
     },
-    getCookie: function (_name) {
-      var value = document.cookie.match("(^|;) ?" + _name + "=([^;]*)(;|$)");
-      return value ? value[2] : null;
+    getSession() {
+      return sessionStorage.getItem("session");
     },
   },
 };
--- src/pages/UserManagementTab.vue
+++ src/pages/UserManagementTab.vue
@@ -5,23 +5,18 @@
         <md-card>
           <md-card-header data-background-color="green">
             <h4 class="title">
-              {{
-                this._props.englishSwitch_Tab ? "User Management" : "유저 관리"
-              }}
+              {{ this._props.englishSwitch ? "User Management" : "유저 관리" }}
             </h4>
             <p class="category">
               {{
-                this._props.englishSwitch_Tab
+                this._props.englishSwitch
                   ? "Card about user management"
                   : "유저 관리 관련 카드"
               }}
             </p>
           </md-card-header>
           <md-card-content>
-            <user-table
-              :userInfo_Table="userInfo_Tab"
-              :englishSwitch_Table="englishSwitch_Tab"
-            ></user-table>
+            <user-table :englishSwitch="englishSwitch"></user-table>
           </md-card-content>
         </md-card>
       </div>
@@ -34,15 +29,10 @@ import { UserTable } from "@/components";
 
 export default {
   props: {
-    userInfo_Tab: Object,
-    englishSwitch_Tab: Boolean,
+    englishSwitch: Boolean,
   },
   components: {
     UserTable,
-  },
-  created() {
-    console.log("UserManagementTab");
-    console.log(this._props.userInfo_Tab);
   },
 };
 </script>
